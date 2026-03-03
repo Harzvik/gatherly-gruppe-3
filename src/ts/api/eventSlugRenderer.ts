57117ceeeb27da13db26cca26d5d12484ff0fb79
Harzvik <harzvik@gmail.com>
2026-03-03 14:22:52 +0100

    Laget eventSlugRenderer, men innser nå at det er en annen funkjson som skal rendre...

--- src/ts/api/eventSlugRenderer.ts
+++ src/ts/api/eventSlugRenderer.ts
@@ -1,15 +1,13 @@
-import type { MeetupsType } from "../types/meetupType.ts";
 import { getMeetupById } from "../api/meetupFetcher.ts";
 
 const queryString = window.location.search;
-console.log(queryString);
-
 const urlParams = new URLSearchParams(queryString);
-console.log(urlParams);
-
 const meetupId = Number(urlParams.get("id"));
+
 console.log(meetupId, typeof meetupId);
 
-/* if (meetupId) {
+if (meetupId) {
   getMeetupById(meetupId);
-} */
+} else {
+  console.error("Ingen gyldig meetup ID funnet i URL-en.");
+}
