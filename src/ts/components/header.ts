e361c34f36866105da528a24a2d5d1619eae7264
Eileen Kim <eileen8kim@gmail.com>
2026-03-03 14:27:03 +0100

    la til klasser i header.ts

--- src/ts/components/header.ts
+++ src/ts/components/header.ts
@@ -1,29 +1,30 @@
 export class HeaderComponent extends HTMLElement {
   connectedCallback(): void {
     this.innerHTML = `
-        <header>
-            <nav>
-            <a href="../index.html">
-                <img src="../public/assets/gatherly_logo.png"/>
-            </a>
-                <ul>
+        <header class="main-header">
+            <nav class="nav-container">
+                <a href="../index.html" class="logo-wrapper">
+                    <img src="../public/assets/gatherly_logo.png" alt="gatherly-logo"/>
+                </a>
+                <ul class="nav-links">
                     <li>
-                        <a href="../index.html">Hjem</a>
+                        <a href="../index.html" class=nav-item active>Hjem</a>
                     </li>
                     <li>
-                        <a href="../utforsk.html">Utforsk</a>
+                        <a href="../utforsk.html" class=nav-item>Utforsk</a>
                     </li>
                     <li>
-                        <a href="../kontakt.html">Kontakt oss</a>
+                        <a href="../kontakt.html" class=nav-item>Kontakt oss</a>
                     </li>
                     <li style="display: none;">
-                        <a href="../minSide.html">Min Side</a>
+                        <a href="../minSide.html" class=nav-item>Min Side</a>
                     </li>
                     <li>
-                        <a href="../loggInn.html">Logg inn</a>
+                        <a href="../loggInn.html" class=nav-item>Logg inn</a>
                     </li>
                 </ul>
             </nav>
+            <hr class="header-divider">
         </header>
     `;
   }
