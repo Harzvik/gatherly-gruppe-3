fec569cc37219668e48abcf52d6553b3962d989a
Eileen Kim <eileen8kim@gmail.com>
2026-03-19 11:23:46 +0100

    card component css + kan merges + branchen kan slettes

--- src/ts/pages/index.ts
+++ src/ts/pages/index.ts
@@ -1,4 +1,6 @@
 /* bruk disse til å importere alle funkjsoner og komponenter for hver side  */
+import { CardComponent } from "../components/card.ts";
 import { HeaderComponent } from "../components/header.ts";
 
 customElements.define("g-header", HeaderComponent);
+customElements.define("g-card", CardComponent);
