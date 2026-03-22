b4c5c2743abc4d9d6a7704792a675a1e11703e3c
Alex <harzvik@gmail.com>
2026-03-22 16:17:32 +0100

    Made first draft of event details card, had to refactor some of the html render to make the tape not be in the mask

--- src/ts/functions/eventSlugRenderer.ts
+++ src/ts/functions/eventSlugRenderer.ts
@@ -12,28 +12,31 @@ export async function renderEventDetails(meetupData: MeetupsType) {
             <p><strong>Opprettet av:</strong> Placeholder bruker </p>
             <p class="event-description">${meetupData.description}</p>
             <div class="event-details-card">
-                <img src="../../../public/assets/component_assets/cyan_tape/tape3.webp" alt="Blå teipbit" class="event-image"/>
-                <img src="../../../public/assets/default_event_img.jpeg" alt="${meetupData.name}" class="event-image"/>
-                <div class="event-date">
-                    <img src="../../../public/assets/component_assets/icons/calender_1.webp" alt="Kalender ikon" class="calender_1"/>
-                    <p>${meetupData.date}</p>
+                <img src="../../../public/assets/component_assets/cyan_tape/tape3.webp" alt="Blå teipbit" class="event-tape"/>
+                <div class="event-details-mask">
+                    <img src="../../../public/assets/default_event_img.jpeg" alt="${meetupData.name}" class="event-image"/>
+                
+                    <div class="event-date">
+                        <img src="../../../public/assets/component_assets/icons/calender_1.webp" alt="Kalender ikon" class="calender_1"/>
+                        <p>${meetupData.date}</p>
+                    </div>
+                    <div class="event-location">
+                        <img src="../../../public/assets/component_assets/icons/location.webp" alt="Sted ikon" class="location_1"/>
+                        <p>${meetupData.location}</p>
+                    </div>
                 </div>
-                <div class="event-location">
-                    <img src="../../../public/assets/component_assets/icons/location.webp" alt="Sted ikon" class="location_1"/>
-                    <p>${meetupData.location}</p>
-                </div>
-            </div>
-            <div class="event-actions-bar">
-                <div class="event-bar-left">
-                    <p>${meetupData.date}</p>
-                    <h2 class="event-title-bar">${meetupData.name}</h2>
-                </div>
-                <div class="event-bar-right">
-                    <span class="event-price">Gratis</span>
-                    <button class="like-event-btn">
-                        <img src="../../../public/assets/component_assets/icons/heart_empty.webp" alt="Hjerte ikon" class="like-event-img"/>
-                    </button>
-                    <button class="join-event-btn">Bli med!</button>
+                <div class="event-actions-bar">
+                    <div class="event-bar-left">
+                        <p>${meetupData.date}</p>
+                        <h2 class="event-title-bar">${meetupData.name}</h2>
+                    </div>
+                    <div class="event-bar-right">
+                        <span class="event-price">Gratis</span>
+                        <button class="like-event-btn">
+                            <img src="../../../public/assets/component_assets/icons/heart_empty.webp" alt="Hjerte ikon" class="like-event-img"/>
+                        </button>
+                        <button class="join-event-btn">Bli med!</button>
+                    </div>
                 </div>
             </div>
 
