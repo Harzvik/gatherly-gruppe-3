29f50d113110f366d773c9edf9e02726ae64bc41
Harzvik <harzvik@gmail.com>
2026-05-15 13:32:51 +0200

    Moved api key to env file

--- src/ts/api/config.ts
+++ src/ts/api/config.ts
@@ -1,4 +1,4 @@
 /*Alex Harsvik*/
 
 export const API_BASE_URL = "http://localhost:3000/api";
-export const API_KEY = "676767"; // Placeholder API key
+export const API_KEY = import.meta.env.VITE_API_KEY;
